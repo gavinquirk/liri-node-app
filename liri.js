9fa15c74a59d4cfc2adf174addd2d8e0b242bb29
Gavin Quirk <gavinquirk@gmail.com>
2018-03-27 23:33:38 -0700

    added file-system functionality

--- liri.js
+++ liri.js
@@ -11,7 +11,7 @@ var pmpt = inq.createPromptModule()
 
 
 
-// TWITTER
+// // TWITTER
 // if (process.argv[2] === 'my-tweets') {
 //     var params = { screen_name: 'gqtw10' };
 //     client.get('statuses/user_timeline', params, function (error, tweets, response) {
@@ -24,26 +24,39 @@ var pmpt = inq.createPromptModule()
 
 //         }
 //     })
+
+//     // SPOTIFY
+// } else if (process.argv[2] === 'spotify-this-song') {
+
+//     var userInput = process.argv[3]
+//     spotify.search({ type: 'track', query: userInput, limit: '1' }, function (err, data) {
+//         if (err) {
+//             return console.log('Error occurred: ' + err);
+//         }
+
+//         console.log('Artist Name: ' + data.tracks.items[0].artists[0].name); //artist name
+//         console.log('Song Name: ' + data.tracks.items[0].name); //song name
+//         console.log('Album Name: ' + data.tracks.items[0].album.name) //album name
+//         console.log('Preview URL: ' + data.tracks.items[0].preview_url); //preview url
+
+//     })
 // }
 
-// SPOTIFY
-spotify.search({ type: 'track', query: 'Airbag', limit: '1' }, function (err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
-
-
-    console.log('Artist Name: ' + data.tracks.items[0].artists[0].name); //artist name
-    console.log('Song Name: ' + data.tracks.items[0].name); //song name
-    console.log('Album Name: ' + data.tracks.items[0].album.name) //album name
-    console.log('Preview URL: ' + data.tracks.items[0].preview_url); //preview url
-
-})
-
 // OMDB
-// var request = require('request');
-// request('http://www.omdbapi.com/?apikey=trilogy&s=goodfellas', function (error, response, body) {
+// var request = require('request')
+// request('http://www.omdbapi.com/?apikey=trilogy&s=goodfellas&r=json&type=movie', function (error, response, body) {
 //   console.log('error:', error); // Print the error if one occurred
-// //   console.log(response)
-//   console.log(JSON.parse(body))
-// });
+//     var parsedData = JSON.parse(body)
+//   console.log(parsedData.title)
+// })
+
+// fs.writeFile('random.txt', 'aaa', function(err) {})
+
+// DO WHAT IT SAYS
+var fs = require('fs')
+  , filename = process.argv[2];
+fs.readFile(filename, 'utf8', function(err, instructions) {
+  if (err) throw err;
+  console.log(instructions)
+
+});
